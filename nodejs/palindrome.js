d2f4d4162a2787a094d56a49f0ea02c7864160b4
victorroli <victorfelipe1608@gmail.com>
2025-02-04 00:17:32 -0300

    palindrome checker in nodejs

--- nodejs/palindrome.js
+++ nodejs/palindrome.js
@@ -1,5 +1,24 @@
 // Palindrome Checker in nodejs
 
-const word = require('readline')
+const readline = require('readline')
 
-console.log(word)
+const question = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function isPalindrome(word) {
+    const cleanedWord = word.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
+    const reversedWord = cleanedWord.split('').reverse().join('')
+    return cleanedWord == reversedWord
+}
+
+question.question("Type a word: ", (word) => {
+
+    if (word.trim == "0") {
+        question.close()
+    }
+
+    console.log(isPalindrome(word) ? "It's a palindrome" : "Not is a palindrome")
+    question.close()
+})
