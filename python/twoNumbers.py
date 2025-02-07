5607246e054e9ec603201bea3c79ec39010f0eb1
victorroli <victorfelipe1608@gmail.com>
2025-02-07 00:13:21 -0300

    Improved display of numbers and positions

--- python/twoNumbers.py
+++ python/twoNumbers.py
@@ -8,34 +8,35 @@ def two_numbers():
 
     list_numbers = entry_list.split(' ')
     
-    print(list_numbers)
-    print(f'Target value: {target_value}')
-
     list_numbers = list(map(int, list_numbers))
-    print(list_numbers)
 
     size_list = len(list_numbers)
     find_number = False
+    positions = []
 
-    for item_pre in range(size_list - 1):
+    for item_pre in range(size_list):
         #print("pre:",item_pre)
         if find_number:
             break
 
         for item_pos in range(size_list - 1):
             
-            if item_pos == item_pre:
+            if list_numbers[item_pre] == list_numbers[item_pos]:
                 break
 
             sum_numbers = list_numbers[item_pre] + list_numbers[item_pos]
             
             if sum_numbers == int(target_value) and not find_number:
                 find_number = True
-                print("Equal numbers...")
-                print(f'Sum numbers of positions: {item_pre, item_pos}')
+                
+                positions.append(item_pre)
+                positions.append(item_pos)
+                
                 break
-            else:
-                pass
+    
+    print("Equal numbers...")
+    print("Positions: ", positions)
+    print(f'Items: {list_numbers[positions[0]]} and {list_numbers[positions[1]]}')
         
         
 
